642b0ee99111daba9b6775e4b190fad928fc05c7
aurimiux <aurimiux.dudux@gmail.com>
2021-09-12 12:12:24 +0300

    Words feature added

--- app/src/main/java/com/example/mipt2/MainActivity.java
+++ app/src/main/java/com/example/mipt2/MainActivity.java
@@ -3,12 +3,94 @@ package com.example.mipt2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spCounterOption;
+    private EditText edEnteredText;
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.spCounterOption = findViewById(R.id.spCounterOption);
+        this.edEnteredText = findViewById(R.id.etEnteredText);
+        this.tvResult = findViewById(R.id.tvResult);
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.selection_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spCounterOption.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+        Log.i("Routine", "onRestart => ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+        Log.i("Routine", "onStart => ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+        Log.i("Routine", "onResume => ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+        Log.i("Routine", "onPause => ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+        Log.i("Routine", "onStop => ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+
+    }
+
+
+    public void btnClick(View view) {
+
+        Toast.makeText(this, this.spCounterOption.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+
+        String spSelectedOption = this.spCounterOption.getSelectedItem().toString();
+        String charString = getString(R.string.selection_chars);
+
+        if(spSelectedOption.equalsIgnoreCase(charString)){
+            String enteredUserText = this.edEnteredText.getText().toString();
+            int charsCount = TextCounter.getCharsCount(enteredUserText);
+
+            this.tvResult.setText(String.valueOf(charsCount));
+        }
+        else{
+            Toast.makeText(this,"Not implemented", Toast.LENGTH_SHORT);
+        }
+
     }
 }
